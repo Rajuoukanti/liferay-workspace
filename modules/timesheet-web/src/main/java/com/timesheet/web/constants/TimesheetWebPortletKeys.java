3048d270958c7bf1120822743d326afcbfe9379e
Rajuoukanti <rajuoukanti1984@gmail.com>
2025-07-24 19:21:18 +0530

    commitetd timesheet-web

---
+++ modules/timesheet-web/src/main/java/com/timesheet/web/constants/TimesheetWebPortletKeys.java
@@ -0,0 +1,11 @@
+package com.timesheet.web.constants;
+
+/**
+ * @author vidyayug
+ */
+public class TimesheetWebPortletKeys {
+
+	public static final String TIMESHEETWEB =
+		"com_timesheet_web_TimesheetWebPortlet";
+
+}
