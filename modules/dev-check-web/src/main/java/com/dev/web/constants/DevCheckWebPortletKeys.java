5ecb9eb9698c7bb88c4fa1d78a591c05ba9fe5d0
Rajuoukanti <rajuoukanti1984@gmail.com>
2025-07-24 15:16:38 +0530

    dev module commit

---
+++ modules/dev-check-web/src/main/java/com/dev/web/constants/DevCheckWebPortletKeys.java
@@ -0,0 +1,11 @@
+package com.dev.web.constants;
+
+/**
+ * @author vidyayug
+ */
+public class DevCheckWebPortletKeys {
+
+	public static final String DEVCHECKWEB =
+		"com_dev_web_DevCheckWebPortlet";
+
+}
