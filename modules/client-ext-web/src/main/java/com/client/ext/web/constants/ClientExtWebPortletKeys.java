89e7f8729efc7dfbfc7e6854c1867a27c5907252
Rajuoukanti <rajuoukanti1984@gmail.com>
2025-07-28 13:56:04 +0530

    Committing the updated client extn

---
+++ modules/client-ext-web/src/main/java/com/client/ext/web/constants/ClientExtWebPortletKeys.java
@@ -0,0 +1,11 @@
+package com.client.ext.web.constants;
+
+/**
+ * @author vidyayug
+ */
+public class ClientExtWebPortletKeys {
+
+	public static final String CLIENTEXTWEB =
+		"com_client_ext_web_ClientExtWebPortlet";
+
+}
---
+++ modules/client-ext-web/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="clientextweb.caption"/></b>
+</p>
