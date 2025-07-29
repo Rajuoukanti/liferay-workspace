f083e5a55646bacd2680611378df0d647e9ea903
Rajuoukanti <rajuoukanti1984@gmail.com>
2025-07-29 12:05:13 +0530

    Module and one client ext comitting

---
+++ modules/insurance-policy-web/src/main/java/com/insurance/policy/web/constants/InsurancePolicyWebPortletKeys.java
@@ -0,0 +1,11 @@
+package com.insurance.policy.web.constants;
+
+/**
+ * @author vidyayug
+ */
+public class InsurancePolicyWebPortletKeys {
+
+	public static final String INSURANCEPOLICYWEB =
+		"com_insurance_policy_web_InsurancePolicyWebPortlet";
+
+}
---
+++ modules/insurance-policy-web/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="insurancepolicyweb.caption"/></b>
+</p>
